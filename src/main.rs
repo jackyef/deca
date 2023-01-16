0d2ec12dc130ce651b2969f95535800e36da9079
jackyef <jacky.efendi1@gmail.com>
2023-01-16 23:52:48 +0700

    Finished ch 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{self, stdout, Read};
+
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(error: std::io::Error) {
+    panic!("{}", error);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for byte in io::stdin().bytes() {
+        match byte {
+            Ok(byte) => {
+                let char = byte as char;
+
+                if char.is_control() {
+                    println!("{:#b} \r", byte)
+                } else {
+                    println!("{:#b} ({})\r", byte, char)
+                }
+
+                if byte == to_ctrl_byte('z') {
+                    break;
+                }
+            },
+            Err(error) => die(error),
+        }
+    }
 }
